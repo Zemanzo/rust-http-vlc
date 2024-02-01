598e2249e441f29871f757888ba77f02cd761f7e
Merlijn van der Kamp <zemanzo.my@gmail.com>
2024-02-01 21:54:16 +0100

    It compiles!

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use futures_util::{SinkExt, StreamExt};
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use std::process::{Command, Stdio};
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::thread;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, Mutex};
 use warp::filters::ws::{Message, WebSocket};
 use warp::Filter;
 
-fn start_vlc_thread(sender: mpsc::Sender<String>, receiver: mpsc::Receiver<Message>) {
+fn start_vlc_thread(sender: mpsc::Sender<String>, mut receiver: mpsc::Receiver<Message>) {
     let vlc_path = Path::new("vlc");
     let child = Command::new(vlc_path)
         .arg("-I")
@@ -21,10 +21,11 @@ fn start_vlc_thread(sender: mpsc::Sender<String>, receiver: mpsc::Receiver<Messa
 
     println!("Started process: {}", child.id());
 
-    thread::spawn(move || async {
+    thread::spawn(|| async move {
         let mut vlc_output = BufReader::new(child.stdout.unwrap());
         let mut stdin = child.stdin.unwrap();
-        while let Some(msg) = receiver.recv().await {
+        while let Some(msg) = receiver.blocking_recv() {
+            println!("received input: {:?}", msg);
             stdin.write_all(msg.as_bytes()).unwrap();
             let mut buf = String::new();
             match vlc_output.read_line(&mut buf) {
@@ -46,10 +47,10 @@ async fn main() {
     if cfg!(target_os = "windows") {
         println!("This application is not supported on Windows: VLC rc does not support stdin input on Windows.");
     }
-    let (input_sender, input_receiver) = mpsc::channel(100);
-    let (output_sender, output_receiver) = mpsc::channel(100);
+    let (input_sender, input_receiver) = mpsc::channel(10000);
+    let (output_sender, output_receiver) = mpsc::channel(10000);
 
-    let output_receiver = Arc::new(Mutex::new(output_receiver));
+    let output_receiver_arc = Arc::new(Mutex::new(output_receiver));
 
     start_vlc_thread(output_sender, input_receiver);
 
@@ -59,11 +60,14 @@ async fn main() {
     let socket = warp::path("socket")
         // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .map(|ws: warp::ws::Ws| {
+        .map(move |ws: warp::ws::Ws| {
+            println!("Connected user!");
+
             // And then our closure will be called when it completes...
-            ws.on_upgrade(move |websocket| {
-                socket_handler(websocket, output_receiver, input_sender.clone())
-            })
+            let output_clone = output_receiver_arc.clone();
+            let input_clone = input_sender.clone();
+
+            ws.on_upgrade(move |websocket| socket_handler(websocket, output_clone, input_clone))
         });
 
     let routes = warp::get().and(root.or(socket));
@@ -77,13 +81,20 @@ async fn socket_handler(
     input_sender: mpsc::Sender<Message>,
 ) {
     let (mut socket_sender, mut socket_receiver) = ws.split();
-    tokio::task::spawn(async move {
-        let mut rec = output_receiver.lock().unwrap();
+    thread::spawn(|| async move {
+        let mut rec = output_receiver.lock().await;
         while let Some(line) = rec.recv().await {
-            socket_sender.send(Message::text(line));
+            let _ = socket_sender.send(Message::text(line));
         }
     });
     while let Some(msg) = socket_receiver.next().await {
-        input_sender.send(msg.unwrap());
+        let unwrapped_msg = msg.unwrap();
+        println!("Recieved ws message: {:?}", unwrapped_msg);
+        if let Err(_) = input_sender.send(unwrapped_msg).await {
+            println!("receiver dropped");
+            return;
+        } else {
+            println!("I sent the thing");
+        }
     }
 }
